57fd6804b3d02ca4ccbed5d15939fb4e7994ed1f
We10Grad <jonzeldenrust@gmail.com>
2025-11-06 22:07:59 -0600

    Update check_required_files.py

--- check_required_files.py
+++ check_required_files.py
@@ -1,10 +1,16 @@
-import os
-import shutil
-
-required_files = ["README.md", ".gitignore"]
-
-
-for filename in required_files:
-    if not os.path.isfile(filename):
-        print("Missing Files")
+def check_required_files():
+    required_files = ["README.md", ".gitignore"]  
+    missing_files = []
+    
+    for required_file in required_files:
+        if not os.path.isfile(required_file):  
+            missing_files.append(required_file)
+    
+    if missing_files:
+        print("Missing Files:", ", ".join(missing_files))
         exit(1)
+    else:
+        exit(0)
+
+if __name__ == "__main__":
+    check_required_files()
